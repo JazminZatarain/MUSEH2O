804b0053357e45d2751dbc1c68a1af5aec68180a
Mark <markwitvliet@hotmail.com>
2021-10-06 22:40:04 +0200

    Update main_susquehanna.py

--- main_susquehanna.py
+++ main_susquehanna.py
@@ -7,13 +7,21 @@
 import numpy as np
 from susquehanna_model import susquehanna_model
 from platypus import Problem, EpsNSGAII, Real, ProcessPoolEvaluator
+import random
+import os
+import csv
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    seeds = [10]  # , 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    for modelseed in seeds:
+        # set seed
+        random.seed(modelseed)
     # RBF parameters
+    RBFType = "SE"
     numberOfInput = 2  # (time, storage of Conowingo)
     numberOfOutput = 4  # Atomic, Baltimore,Chester, Downstream:- hydropower, environmental
     numberOfRBF = 4  # numberOfInput + 2
@@ -26,8 +34,8 @@ def main():
     susquehanna_river = susquehanna_model(108.5, 505.0, 5, n_years)  # l0, l0_MR, d0, years
     # l0 = start level cono, l0_MR = start level muddy run, d0 = startday > friday = 5
     susquehanna_river.load_data(0)  # 0 = historic, 1 = stochastic
-
-    susquehanna_river.setRBF(numberOfRBF, numberOfInput, numberOfOutput)
+    susquehanna_river.set_log(False)
+    susquehanna_river.setRBF(numberOfRBF, numberOfInput, numberOfOutput, RBFType)
 
     # Lower and Upper Bound for problem.types
     LB = [-1, 0, -1, 0, 0, 0, 0, 0] * numberOfRBF
@@ -52,18 +60,32 @@ def main():
 
     # algorithm = NSGAII(problem)
     # algorithm.run(1)
-    with ProcessPoolEvaluator(4) as evaluator:
+    with ProcessPoolEvaluator() as evaluator:
         algorithm = EpsNSGAII(problem, epsilons=EPS, evaluator=evaluator)
-        algorithm.run(100)
+        algorithm.run(1000)
 
     # results
     print("results:")
     for solution in algorithm.result:
         print(solution.objectives)
-    with open("./output/output.txt", "w") as f:
-        for solution in algorithm.result:
-            f.write("%s\n" % solution.objectives)
+
+#     header = ["hydropower", "atomicpowerplant", "baltimore", "chester", "environment", "recreation"]
+#     with open(f"output/{RBFType}_{modelseed}_solution.csv", "w", encoding="UTF8", newline="") as f:
+#         writer = csv.writer(f)
+#         writer.writerow(header)
+#         for solution in algorithm.result:
+#             writer.writerow(solution.objectives)
+
+#     with open(f"output/{RBFType}_{modelseed}_variables.csv", "w", encoding="UTF8", newline="") as f:
+#         writer = csv.writer(f)
+#         for solution in algorithm.result:
+#             writer.writerow(solution.variables)
 
 
 if __name__ == "__main__":
+    if not os.path.exists("output"):
+        try:
+            os.mkdir("output")
+        except OSError:
+            print("Creation of the directory failed")
     main()
