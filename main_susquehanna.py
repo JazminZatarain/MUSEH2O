3cba8267d068cd66f6c4883f8cc044a507441100
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2021-10-08 07:54:12 +0200

    new results

--- main_susquehanna.py
+++ main_susquehanna.py
@@ -16,70 +16,70 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    seeds = [10]  # , 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    seeds = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     for modelseed in seeds:
         # set seed
         random.seed(modelseed)
-    # RBF parameters
-    RBFType = "SE"
-    numberOfInput = 2  # (time, storage of Conowingo)
-    numberOfOutput = 4  # Atomic, Baltimore,Chester, Downstream:- hydropower, environmental
-    numberOfRBF = 4  # numberOfInput + 2
-    # N = 2 * n * m + K * n  # check 32 with 2 inputs, 72 with 4 inputs
+        # RBF parameters
+        RBFType = "SE"
+        numberOfInput = 2  # (time, storage of Conowingo)
+        numberOfOutput = 4  # Atomic, Baltimore,Chester, Downstream:- hydropower, environmental
+        numberOfRBF = 4  # numberOfInput + 2
+        # N = 2 * n * m + K * n  # check 32 with 2 inputs, 72 with 4 inputs
 
-    # Initialize model
-    nobjs = 6
-    nvars = int(numberOfRBF * 8)  # 8 = 2 centers + 2 radius + 4 weights
-    n_years = 1
-    susquehanna_river = susquehanna_model(108.5, 505.0, 5, n_years)  # l0, l0_MR, d0, years
-    # l0 = start level cono, l0_MR = start level muddy run, d0 = startday > friday = 5
-    susquehanna_river.load_data(0)  # 0 = historic, 1 = stochastic
-    susquehanna_river.set_log(False)
-    susquehanna_river.setRBF(numberOfRBF, numberOfInput, numberOfOutput, RBFType)
+        # Initialize model
+        nobjs = 6
+        nvars = int(numberOfRBF * 8)  # 8 = 2 centers + 2 radius + 4 weights
+        n_years = 1
+        susquehanna_river = susquehanna_model(108.5, 505.0, 5, n_years)  # l0, l0_MR, d0, years
+        # l0 = start level cono, l0_MR = start level muddy run, d0 = startday > friday = 5
+        susquehanna_river.load_data(0)  # 0 = historic, 1 = stochastic
+        susquehanna_river.set_log(False)
+        susquehanna_river.setRBF(numberOfRBF, numberOfInput, numberOfOutput, RBFType)
 
-    # Lower and Upper Bound for problem.types
-    LB = [-1, 0, -1, 0, 0, 0, 0, 0] * numberOfRBF
-    UB = [1, 1, 1, 1, 1, 1, 1, 1] * numberOfRBF
-    # np.pi*2 for phaseshift upperbounds (J. Quinn Como model) check borg optimization_serial flood C model
-    EPS = [0.5, 0.05, 0.05, 0.05, 0.05, 0.001]
+        # Lower and Upper Bound for problem.types
+        LB = [-1, 0, -1, 0, 0, 0, 0, 0] * numberOfRBF
+        UB = [1, 1, 1, 1, 1, 1, 1, 1] * numberOfRBF
+        # np.pi*2 for phaseshift upperbounds (J. Quinn Como model) check borg optimization_serial flood C model
+        EPS = [0.5, 0.05, 0.05, 0.05, 0.05, 0.001]
 
-    # platypus MOEA, no contraints
-    problem = Problem(nvars, nobjs)
-    # problem.types[:] = Real(-1, 1)
-    problem.types[:] = [Real(LB[i], UB[i]) for i in range(nvars)]
-    problem.function = susquehanna_river.evaluate  # historical (deterministic) optimization
-    # problem.function = functools.partial(susquehanna_river.evaluates, opt_met=1) #way to add arguments
-    # problem.function = susquehanna_river.evaluateMC  # stochastic optimization
-    # problem.directions[:] = Problem.MINIMIZE
-    problem.directions[0] = Problem.MINIMIZE  # hydropower
-    problem.directions[1] = Problem.MINIMIZE  # atomicpowerplant
-    problem.directions[2] = Problem.MINIMIZE  # baltimore
-    problem.directions[3] = Problem.MINIMIZE  # chester
-    problem.directions[4] = Problem.MAXIMIZE  # environment
-    problem.directions[5] = Problem.MINIMIZE  # recreation
+        # platypus MOEA, no contraints
+        problem = Problem(nvars, nobjs)
+        # problem.types[:] = Real(-1, 1)
+        problem.types[:] = [Real(LB[i], UB[i]) for i in range(nvars)]
+        problem.function = susquehanna_river.evaluate  # historical (deterministic) optimization
+        # problem.function = functools.partial(susquehanna_river.evaluates, opt_met=1) #way to add arguments
+        # problem.function = susquehanna_river.evaluateMC  # stochastic optimization
+        # problem.directions[:] = Problem.MINIMIZE
+        problem.directions[0] = Problem.MINIMIZE  # hydropower
+        problem.directions[1] = Problem.MINIMIZE  # atomicpowerplant
+        problem.directions[2] = Problem.MINIMIZE  # baltimore
+        problem.directions[3] = Problem.MINIMIZE  # chester
+        problem.directions[4] = Problem.MAXIMIZE  # environment
+        problem.directions[5] = Problem.MINIMIZE  # recreation
 
-    # algorithm = NSGAII(problem)
-    # algorithm.run(1)
-    with ProcessPoolEvaluator() as evaluator:
-        algorithm = EpsNSGAII(problem, epsilons=EPS, evaluator=evaluator)
-        algorithm.run(50000)
+        # algorithm = NSGAII(problem)
+        # algorithm.run(1)
+        with ProcessPoolEvaluator() as evaluator:
+            algorithm = EpsNSGAII(problem, epsilons=EPS, evaluator=evaluator)
+            algorithm.run(100000)
 
-    # results
-    print("results:")
-    for solution in algorithm.result:
-        print(solution.objectives)
-
-    header = ["hydropower", "atomicpowerplant", "baltimore", "chester", "environment", "recreation"]
-    with open(f"output/{RBFType}_{modelseed}_solution.csv", "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
+        # results
+        print("results:")
         for solution in algorithm.result:
-            writer.writerow(solution.objectives)
+            print(solution.objectives)
 
-    with open(f"output/{RBFType}_{modelseed}_variables.csv", "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        for solution in algorithm.result:
-            writer.writerow(solution.variables)
+        header = ["hydropower", "atomicpowerplant", "baltimore", "chester", "environment", "recreation"]
+        with open(f"output/{RBFType}_{modelseed}_solution.csv", "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            for solution in algorithm.result:
+                writer.writerow(solution.objectives)
+
+        with open(f"output/{RBFType}_{modelseed}_variables.csv", "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            for solution in algorithm.result:
+                writer.writerow(solution.variables)
 
 
 if __name__ == "__main__":
