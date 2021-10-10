5cdd5ad529b1a972ebc7e0da575da0ef566af3a2
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2021-10-10 21:20:52 +0200

    Update susquehanna_model.py

--- susquehanna_model.py
+++ susquehanna_model.py
@@ -132,7 +132,7 @@ class SusquehannaModel:
             create_path("./data1999/wAtomic.txt"), self.n_days_one_year
         )  # water demand for cooling the atomic power plant (cfs)
 
-        # standardization of the input-outpu of the RBF release curve
+        # standardization of the input-output of the RBF release curve
         self.input_max.append(self.n_days_in_year * self.day_fraction - 1)
         # self.input_max.append(6) commented out in c model
         self.input_max.append(120)
