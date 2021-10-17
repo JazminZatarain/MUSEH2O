5909d3ea4197d2215ce9b9a4b7581dc97bd7ee8a
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2021-10-17 09:59:42 +0200

    new results

--- main_susquehanna.py
+++ main_susquehanna.py
@@ -87,7 +87,7 @@ def main():
 
         with ProcessPoolEvaluator() as evaluator: #change to number of threads
             algorithm = EpsNSGAII(problem, epsilons=eps, evaluator=evaluator)
-            algorithm.run(1000)
+            algorithm.run(100000)
 
         header = ['hydropower', 'atomicpowerplant', 'baltimore', 'chester',
                   'environment', 'recreation']
