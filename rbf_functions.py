9361a8395a7c5d4d45bb3cddf42172389099f909
Mark <markwitvliet@hotmail.com>
2021-12-01 10:52:39 +0100

    fixed matern32, added matern52, notebook analysis changes

--- rbf_functions.py
+++ rbf_functions.py
@@ -202,8 +202,7 @@ def multi_quadric2_rbf(rbf_input, centers, radii, weights):
 
     """
 
-    rbf_scores = np.sqrt(np.sum((radii ** 2) + ((rbf_input - centers) ** 2),
-                                axis=1))
+    rbf_scores = np.sqrt(np.sum((radii ** 2) + ((rbf_input - centers) ** 2), axis=1))
     weighted_rbfs = weights * rbf_scores[:, np.newaxis]
     output = weighted_rbfs.sum(axis=0)
     return output
@@ -228,7 +227,7 @@ def matern32_rbf(rbf_input, centers, radii, weights):
 
     """
     distances = cdist(rbf_input[np.newaxis, :], centers)
-    sqrt = np.sqrt(3 * np.sum(distances.T/radii, axis=1))
+    sqrt = np.sqrt(3 * np.sum(distances.T / radii, axis=1))
     rbf_scores = (1 + sqrt) * (np.exp(-sqrt))
 
     weighted_rbfs = weights * rbf_scores[:, np.newaxis]
@@ -243,20 +242,113 @@ def matern32_rbf(rbf_input, centers, radii, weights):
     return output
 
 
-rbfs = [squared_exponential_rbf,
-        gaussian_rbf,
-        multiquadric_rbf,
-        inverse_multiquadric_rbf,
-        inverse_quadric_rbf,
-        exponential_rbf,
-        multi_quadric2_rbf,
-        matern32_rbf]
+def matern32_edit(rbf_input, centers, radii, weights):
+    """
+
+    Parameters
+    ----------
+    rbf_input : numpy array
+                1-D, shape is (n_inputs,)
+    centers :   numpy array
+                2-D, shape is (n_rbfs X n_inputs)
+    radii :     2-D, shape is (n_rbfs X n_inputs)
+    weights :   2-D, shape is (n_rbfs X n_outputs)
+
+    Returns
+    -------
+    numpy array
+
+
+    """
+    distances = cdist(rbf_input[np.newaxis, :], centers)
+    sqrt = np.sqrt(3) * np.sum(distances.T / radii, axis=1)
+    rbf_scores = (1 + sqrt) * (np.exp(-sqrt))
+
+    weighted_rbfs = weights * rbf_scores[:, np.newaxis]
+    output = weighted_rbfs.sum(axis=0)
+    # diff = rbf_input - centers
+    # squared = (diff / radii) ** 2  # TODO:: hacked to make it work
+    # sqrt = np.sqrt(3 * np.sum(squared, axis=1))
+    # rbf_scores = (1 + sqrt) * (np.exp(-sqrt))
+    #
+    # weighted_rbfs = weights * rbf_scores[:, np.newaxis]
+    # output = weighted_rbfs.sum(axis=0)
+    return output
+
+
+def matern52_rbf(rbf_input, centers, radii, weights):
+    """
+
+    Parameters
+    ----------
+    rbf_input : numpy array
+                1-D, shape is (n_inputs,)
+    centers :   numpy array
+                2-D, shape is (n_rbfs X n_inputs)
+    radii :     2-D, shape is (n_rbfs X n_inputs)
+    weights :   2-D, shape is (n_rbfs X n_outputs)
+
+    Returns
+    -------
+    numpy array
+
+
+    """
+    distances = cdist(rbf_input[np.newaxis, :], centers)
+    sqrt = np.sqrt(5 * np.sum(distances.T / radii, axis=1))
+    sq = np.square(5 * np.sum(distances.T / 3 * np.square(radii), axis=1))
+    rbf_scores = (1 + sqrt + sq) * (np.exp(-sqrt))
+
+    weighted_rbfs = weights * rbf_scores[:, np.newaxis]
+    output = weighted_rbfs.sum(axis=0)
+    return output
+
+
+def matern52_edit(rbf_input, centers, radii, weights):
+    """
+
+    Parameters
+    ----------
+    rbf_input : numpy array
+                1-D, shape is (n_inputs,)
+    centers :   numpy array
+                2-D, shape is (n_rbfs X n_inputs)
+    radii :     2-D, shape is (n_rbfs X n_inputs)
+    weights :   2-D, shape is (n_rbfs X n_outputs)
+
+    Returns
+    -------
+    numpy array
+
+
+    """
+    distances = cdist(rbf_input[np.newaxis, :], centers)
+    sqrt = np.sqrt(5) * np.sum(distances.T / radii, axis=1)
+    sq = 5 * np.sum(np.square(distances.T) / 3 * np.square(radii), axis=1)
+    rbf_scores = (1 + sqrt + sq) * (np.exp(-sqrt))
+
+    weighted_rbfs = weights * rbf_scores[:, np.newaxis]
+    output = weighted_rbfs.sum(axis=0)
+    return output
+
+
+rbfs = [
+    squared_exponential_rbf,
+    gaussian_rbf,
+    multiquadric_rbf,
+    inverse_multiquadric_rbf,
+    inverse_quadric_rbf,
+    exponential_rbf,
+    multi_quadric2_rbf,
+    matern32_rbf,
+    matern32_edit,
+    matern52_rbf,
+    matern52_edit,
+]
 
 
 class RBF:
-
-    def __init__(self, n_rbfs, n_inputs, n_outputs,
-                 rbf_function=squared_exponential_rbf):
+    def __init__(self, n_rbfs, n_inputs, n_outputs, rbf_function=squared_exponential_rbf):
         self.n_rbfs = n_rbfs
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
@@ -291,12 +383,9 @@ class RBF:
     def set_decision_vars(self, decision_vars):
         decision_vars = decision_vars.copy()
 
-        self.centers = decision_vars[self.c_i].reshape((self.n_rbfs,
-                                                        self.n_inputs))
-        self.radii = decision_vars[self.r_i].reshape((self.n_rbfs,
-                                                      self.n_inputs))
-        self.weights = decision_vars[self.w_i].reshape((self.n_rbfs,
-                                                        self.n_outputs))
+        self.centers = decision_vars[self.c_i].reshape((self.n_rbfs, self.n_inputs))
+        self.radii = decision_vars[self.r_i].reshape((self.n_rbfs, self.n_inputs))
+        self.weights = decision_vars[self.w_i].reshape((self.n_rbfs, self.n_outputs))
 
         # sum of weights per input is 1
         self.weights /= self.weights.sum(axis=0)[np.newaxis, :]
@@ -305,6 +394,7 @@ class RBF:
         outputs = self.rbf(inputs, self.centers, self.radii, self.weights)
 
         return outputs
+
 
 # # @numba.jit
 # def format_output(output, weights):
